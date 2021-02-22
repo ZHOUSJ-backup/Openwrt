86cdd0c21683320e3b0cd200c2e25a3bb73db0a0
ZHOUSJ <46296948+ZHOUSJ6@users.noreply.github.com>
2021-02-22 10:40:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,13 +11,11 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 git clone https://github.com/kenzok8/small.git package/small
 
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb #git lua-maxminddb 依赖
-
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
